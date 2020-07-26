69753830564dd680b7f22449bb6e68d92eac38e2
jlopezt <32620729+jlopezt@users.noreply.github.com>
2020-07-26 11:29:05 +0200

    Incluyo el resto de directorios de de VSC

---
+++ lib/esp8266-google-tts/src/google-tts.h
@@ -0,0 +1,37 @@
+#ifndef GoogleTTS_h
+#define GoogleTTS_h
+
+#include <WiFiClientSecure.h>
+#ifdef ARDUINO_ARCH_ESP8266
+#include "esp8266sdk_version.h"
+#endif
+
+#define LIB_NAME "GoogleTTS for ESP8266"
+#define LIB_VERSION "1.0.7"
+
+#define HOST_GTRANS "translate.google.com"
+#define PATH_GTRANS "/translate_tts"
+#define FINGERPRINT_GTRANS "F8:1E:31:71:FA:08:5B:C0:4C:83:B6:64:4B:9F:22:9F:0C:BA:8E:57"
+
+typedef class GoogleTTS {
+
+private:
+  WiFiClientSecure* m_pClient = nullptr;
+  unsigned long m_lastTimestamp = 0;
+  String m_tkk;
+
+  String getTKK();
+  String createToken(const char* text, const char* key);
+  String urlencode(String str);
+  char* lltoa(long long val, int base);
+  long long int  XL (long long int a, char* b);
+  
+public:
+  void setWiFiClientSecure(WiFiClientSecure* pClient);
+  String getSpeechUrl(String text, String lang);
+  String getSpeechUrl(String text) {
+    return getSpeechUrl(text, "en");
+  }
+} TTS;
+
+#endif
